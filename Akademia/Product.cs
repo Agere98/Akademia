7c2c284458004f8662cf556af16db3e882409960
Agere <bmateusz1998@gmail.com>
2019-11-05 21:44:20 +0100

    Live coding

--- Akademia/Product.cs
+++ Akademia/Product.cs
@@ -2,12 +2,12 @@
 
     public class Product {
 
-        public string name;
-        public decimal price;
+        public string Name { get; set; }
+        public virtual decimal Price { get; set; }
 
         public Product(string name, decimal price) {
-            this.name = name;
-            this.price = price;
+            Name = name;
+            Price = price;
         }
     }
 }
--- Akademia/Vegetable.cs
+++ Akademia/Vegetable.cs
@@ -1,11 +1,22 @@
 ﻿namespace Akademia {
 
-    public class Vegetable : Product {
+    public class Vegetable : Product, ISoldByWeight {
 
-        public float weight;
+        public float Weight { get; set; }
+
+        public override decimal Price {
+            get {
+                // Cena = cena za kilogram * waga
+                return decimal.Round(base.Price * (decimal)Weight, 2);
+            }
+        }
 
         public Vegetable(string name, decimal price, float weight) : base(name, price) {
-            this.weight = weight;
+            Weight = weight;
+        }
+
+        public decimal GetPricePerKilogram() {
+            return base.Price;
         }
     }
 }
